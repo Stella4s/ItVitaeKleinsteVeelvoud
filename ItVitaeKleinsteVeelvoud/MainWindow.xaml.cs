3f1eb615e1dba2a8589cda0d95323bf371f63535
Stella van der burg <luna4s108@gmail.com>
2020-06-12 12:09:09 +0200

    - Added Prime Sieve. - Added part of base lay-out. (not finished)

--- ItVitaeKleinsteVeelvoud/MainWindow.xaml.cs
+++ ItVitaeKleinsteVeelvoud/MainWindow.xaml.cs
@@ -20,9 +20,39 @@ namespace ItVitaeKleinsteVeelvoud
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<int> Primes;
         public MainWindow()
         {
             InitializeComponent();
+            GetPrimes();
+        }
+
+        public void GetPrimes()
+        {
+            int x = 100;
+            Primes = new List<int> { 2 };
+            for (int i = 3; i < x; i += 2)
+            {
+                Primes.Add(i);
+            }
+            for (int i = 3; i < (x/2); i += 2)
+            {
+                if (Primes.Contains(i))
+                {
+                    for (int j = 2 * i; j < x; j += i)
+                    {
+                        Primes.Remove(j);
+                    }
+                }
+            }
+        }
+
+        private void Btn_Click_ShowPrimes(object sender, RoutedEventArgs e)
+        {
+            foreach (int prime in Primes)
+            {
+                TextBlock1.Text += " " + prime.ToString();
+            }
         }
     }
 }
